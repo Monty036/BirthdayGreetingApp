4b7a7a10eb01b03701a4500468e07a92d58d758c
AmanMohanty <amanmohanty036@gmail.com>
2023-06-11 15:24:44 +0530

    First Commit

---
+++ app/src/main/java/com/example/birthdaygreeting/BirthdayGreetingActivity.kt
@@ -0,0 +1,21 @@
+package com.example.birthdaygreeting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+
+    companion object{
+        const val NAME_EXTRA="name_extra"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+        val name=intent.getStringExtra(NAME_EXTRA)
+
+        val textview = findViewById<TextView>(R.id.birthdayGreeting)
+        textview.text="Happy Birthday \n$name!"
+    }
+}
